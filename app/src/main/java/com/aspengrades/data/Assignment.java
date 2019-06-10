1789228ab75a1f879ccfbdc9497b2b9887db9b0a
Hank Stennes <hpstennes@gmail.com>
2019-06-09 20:10:03 -0500

    Remove unused fields from Assignment

--- app/src/main/java/com/aspengrades/data/Assignment.java
+++ app/src/main/java/com/aspengrades/data/Assignment.java
@@ -7,27 +7,8 @@ import org.jsoup.nodes.Element;
 public class Assignment {
 
     private String name;
-    private String dateAsgn;
-    private String dateDue;
     private String category;
-    private float weight;
     private String score;
-    private String feedback;
-
-    public Assignment(Element row){
-        name = row.child(1).child(0).text();
-        dateAsgn = row.child(2).text();
-        dateDue = row.child(3).text();
-        category = row.child(4).text();
-        weight = Float.parseFloat(row.child(5).text());
-        if (row.child(7).childNodeSize() == 1) {
-            score = row.child(8).child(0).text();
-            feedback = row.child(9).text();
-        } else {
-            score = row.child(7).child(0).text();
-            feedback = row.child(8).text();
-        }
-    }
 
     public Assignment(Element row, int nameIndex, int categoryIndex, int scoreIndex){
         name = row.child(nameIndex).text();
@@ -39,33 +20,17 @@ public class Assignment {
         return name;
     }
 
-    public String getDateAsgn() {
-        return dateAsgn;
-    }
-
-    public String getDateDue() {
-        return dateDue;
-    }
-
     public String getCategory() {
         return category;
-    }
-
-    public float getWeight() {
-        return weight;
     }
 
     public String getScore() {
         return score;
     }
 
-    public String getFeedback() {
-        return feedback;
-    }
-
     @Override
     @NonNull
     public String toString(){
-        return name + ", " + dateAsgn + ", " + dateDue + ", " + category + ", " + weight + ", " + score + ", " + feedback;
+        return name + ", " + category + ", " + score;
     }
 }
